be39936010fcf2cecf6d0a32a6654d7899dd29bc
carlier.quentin <carlier.quentin@gmail.com>
2022-06-08 14:59:03 +0200

    service-modal

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
+  // providers: [ModalService],
 })
 export class ModalComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(modal: ModalService) {
   }
 
+  ngOnInit(): void {}
 }
