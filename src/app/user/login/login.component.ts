980eecdd3020707aa4cd71d63d0ae8cd7bd48cdf
carlier.quentin <carlier.quentin@gmail.com>
2022-06-15 10:51:09 +0200

    template-forms

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  credentials = {
+    email: '',
+    password: '',
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login() {
+    console.log('submit values', this.credentials);
   }
-
 }
