c88d2d04d3c0845f956d7e63ef93dd2aa0f65568
carlier.quentin <carlier.quentin@gmail.com>
2022-07-07 17:31:21 +0200

    upload-video-ok

--- src/app/pages/upload/upload.component.ts
+++ src/app/pages/upload/upload.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-upload',
@@ -9,24 +11,35 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class UploadComponent {
   isDragover = false;
   file: File | null = null;
-  nextStep = false;
+  fileSelected = false;
 
-  title = new FormControl('', [Validators.required, Validators.min(3)]);
+  title = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+  });
 
   uploadForm = new FormGroup({
     title: this.title,
   });
 
+  constructor(private storage: AngularFireStorage) {}
+
   storeFile(event: Event) {
     this.isDragover = false;
     this.file = (event as DragEvent).dataTransfer?.files.item(0) ?? null;
 
-    if (!this.file || this.file.type !== 'image/mp4') return;
-
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
     this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
+    this.fileSelected = true;
   }
 
   uploadFile() {
     console.log('File uploaded');
+    const clipFileName = uuid();
+    const clipPath = `clips/${clipFileName}.mp4`;
+
+    this.storage.upload(clipPath, this.file);
   }
 }
